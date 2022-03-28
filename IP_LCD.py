02f53d9df6c6f198b37121eea3bad874ccce8594
Omer Shoseyov <omer.shoseyov@gmail.com>
2022-03-28 14:53:11 +0300

    show ip on lcd

---
+++ IP_LCD.py
@@ -0,0 +1,25 @@
+import drivers
+from time import sleep
+from subprocess import check_output
+import os
+
+display = drivers.Lcd()
+first_run = False
+
+
+try:
+    while True:
+        if len(check_output(["hostname", "-I"]).split()):
+                #print("Got IP")
+                IP = check_output(["hostname", "-I"]).split()[0].decode('UTF-8')
+                display.lcd_display_string("         ", 2)
+                display.lcd_display_string("IP:" + str(IP), 2)
+        else:
+                #print("No IP")
+                display.lcd_display_string("  Offline  ", 2)
+
+        sleep(10)
+        display.lcd_clear()
+
+except KeyboardInterrupt:
+    display.lcd_clear()
